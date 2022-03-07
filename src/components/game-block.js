41d74984518d3a8533cb1e0ed83ab765976d79d1
Adam Kendall <timeexe@pop-os.localdomain>
2022-03-06 20:41:16 -0800

    added basic mapping

--- src/components/game-block.js
+++ src/components/game-block.js
@@ -2,6 +2,49 @@ import React from "react";
 
 export const GameBlock = () => {
 
+    const data = [
+        {
+            id: 'dark-souls',
+            imgSrc: 'https://tinyurl.com/4dwmse5a',
+            gameText: 'Dark Souls 1',
+        },
+        {
+            id: 'kotor',
+            imgSrc: 'https://tinyurl.com/bpastw7z',
+            gameText: 'Star Wars: Knights of the Old Republic',
+        },
+        {
+            id: 'metal-gear-solid',
+            imgSrc: 'https://tinyurl.com/4cwkacs7',
+            gameText: 'Metal Gear Solid',
+        },
+    ];
+
+    return (
+        <div className="game-block">
+            {
+                data.map((game) => {
+                    const { id, imgSrc, gameText } = game;
+
+                    return (
+                        <div id={id} className='game-block-info'>
+                            <div className="game-block-img">
+                                <img className="game-block-game-img" src={imgSrc} />
+                            </div>
+                            <div className="game-block-text">
+                                <h1>{gameText}</h1>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
+
+
+        </div>
+    )
+
+    /*
     return (
         <div className="game-block">
             <div id="dark-souls"></div>
@@ -35,4 +78,5 @@ export const GameBlock = () => {
         </div>
 
     )
+    */
 }
