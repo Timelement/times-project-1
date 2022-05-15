5ad1f04c1449057fdf4bd2beaa1fd2b7d850fea5
Timelement <timeexe@gmail.com>
2022-05-15 16:23:21 -0700

    updating input form

--- src/components/input-form.jsx
+++ src/components/input-form.jsx
@@ -5,13 +5,20 @@ import React, { useState, useEffect} from 'react'
 
 export const InputForm = () => {
 
-    const {userName, setUserName} = useState({
+    const [userName, setUserName] = useState({
         name: '',
     });
 
     const handleChange = (event) => {
         setUserName({...userName, [event.target.name]: event.target.value});
     };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(userName);
+        setUserName({name:''})
+    };
+
 
 
     return (
@@ -21,7 +28,7 @@ export const InputForm = () => {
                         <h3>Name:</h3>
                         <input type='text' name='name' value={userName.name}/>
                     
-                    <input type='submit' value='Submit' />
+                    <input type='submit' value='Submit' onClick={() => handleSubmit()}/>
             
             </div>
     )
