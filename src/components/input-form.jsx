bfe20c1bbeea1fdc9ae737d5b2289e4f65b629ea
Timelement <timeexe@gmail.com>
2022-05-15 18:15:40 -0700

    input form now updates states, added debounce function

--- src/components/input-form.jsx
+++ src/components/input-form.jsx
@@ -9,6 +9,8 @@ export const InputForm = () => {
         name: '',
     });
 
+    const [displayState, setDisplayState] = useState('');
+
     const handleChange = (event) => {
         setUserName({...userName, [event.target.name]: event.target.value});
     };
@@ -16,9 +18,29 @@ export const InputForm = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(userName);
-        setUserName({name:''})
+        setDisplayState(userName.name);
+        // setUserName({name:''})
+
+        // let userNameDiv = document.getElementById('user-name-hidden')
+        // if (userNameDiv.style.display === "none") {
+        //     userNameDiv.style.display = "block";
+        //     console.log('time');
+        // } else {
+        //     userNameDiv.style.display = "none";
+        //     console.log('jong');
+        // }
     };
 
+    const debounceEvent = (callback, time) => {
+        let interval;
+        return (...args) => {
+            clearTimeout(interval);
+            interval = setTimeout(() => {
+                interval = null;
+                callback(...args);
+            }; time);
+        };
+    };
 
 
     return (
@@ -26,10 +48,11 @@ export const InputForm = () => {
                 
                     
                         <h3>Name:</h3>
-                        <input type='text' name='name' value={userName.name}/>
+                        <input type='text' name='name' value={userName.name} onChange={handleChange}/>
                     
-                    <input type='submit' value='Submit' onClick={() => handleSubmit()}/>
-            
+                    <input type='submit' value='Submit' onClick={(event) => handleSubmit(event)}/>
+
+                    <div className='userNameDisplay'>{displayState}</div>
             </div>
     )
 
